007e20fb53d4cea8c3bd009551ea4a57b29e8dab
timm <timm@ieee.org>
2025-11-30 15:05:53 -0500

    saving

--- src/binr.lua
+++ src/binr.lua
@@ -14,7 +14,7 @@ Options:
   -f  file=../data/auto93.csv  ]]
 
 -- coerce(s) --> v ;; Return int or float or bool or string from `s`.
-local function coerce(s) 
+local function coerce(s)
   if s then return tonumber(s) or s:match'^%s*(.-)%s*$' end end
 
 local the={}; for k,v in help:gmatch("(%S+)=(%S+)") do the[k] = coerce(v) end
@@ -29,7 +29,7 @@ local floor,min,max,rand,cos = math.floor,math.min,math.max, math.random, math.c
 local say,fmt = io.write, string.format
 
 -- sort(a,f) --> a ;; Sort `a` using function `f`.
-local sort = function(a,f) table.sort(a,f); return a end 
+local sort = function(a,f) table.sort(a,f); return a end
 
 
 -- o(v|t) --> s ;; Return a string representation of `v`.
@@ -38,7 +38,7 @@ local function o(v,     list,dict)
   dict=function(d,u)
      for k,v in pairs(d) do u[1+#u]=fmt(":%s %s",k,o(v)) end; return sort(u) end
   return type(v) == "number" and fmt(v%1==0 and "%.0f" or "%.3f", v) or
-         type(v) ~= "table" and tostring(v) or 
+         type(v) ~= "table" and tostring(v) or
          "{".. table.concat((#v>0 and list or dict)(v,{}), " ") .."}" end
 
 -- s2a(s) --> a ;; Return array of words from string `s`, split on ",".
@@ -56,7 +56,7 @@ local shuffle = function(t,    n)
 	for m=#t,2,-1 do n=math.random(m); t[m],t[n]=t[n],t[m] end; return t end
 
 -- box_muller(mu:,sd:n) --> n ;; Return a random number from a Gaussian `mu`,`sd`.
-local function box_muller(mu,sd) 
+local function box_muller(mu,sd)
   return mu + sd * sqrt(-2 * log(rand())) * cos(2 * math.pi * rand()) end
 
 --## Classes
@@ -68,7 +68,7 @@ function DATA(  src) return adds(src, {n=0,rows={},cols=nil}) end
 function clone(data,  src) return adds(src, DATA{data.cols.names}) end
 
 -- NUM(at=0,v="") --> NUM ;; Create a NUM object to summarize numbers.
-function NUM(at,v) 
+function NUM(at,v)
   return {at=at or 0, of=v or "", n=0, mu=0, m2=0, sd=0, bins={},
           best=(tostring(v) or ""):find"+$" and 1 or 0} end
 
@@ -82,7 +82,7 @@ function COLS(row,    t,x,y,all)
     all[n] = (s:match"^[A-Z]" and NUM or SYM)(n,s)
     if not s:match"X$" then
       t = s:find"[+-]$" and y or x
-      t[1+#t] = all[n] end end 
+      t[1+#t] = all[n] end end
   return {all=all, x=x, y=y, names=row} end
 
 --## Methods
@@ -91,22 +91,22 @@ function COLS(row,    t,x,y,all)
 local function add(i,z)
   if z == "?" then return z end
   i.n = i.n + 1
-  if i.has then i.has[z] = 1 + (i.has[z] or 0) 
-  elseif i.mu then 
+  if i.has then i.has[z] = 1 + (i.has[z] or 0)
+  elseif i.mu then
     local d = z - i.mu
     i.mu = i.mu + d / i.n
     i.m2 = i.m2 + d * (z - i.mu)
-    i.sd = i.n<2 and 0 or sqrt((max(0,i.m2)/(i.n - 1))) 
-  elseif i.rows then  
+    i.sd = i.n<2 and 0 or sqrt((max(0,i.m2)/(i.n - 1)))
+  elseif i.rows then
     if not i.cols then i.cols = COLS(z) else
-      for _,col in pairs(i.cols.all) do add(col, z[col.at]) end 
+      for _,col in pairs(i.cols.all) do add(col, z[col.at]) end
       i.rows[1 + #i.rows] = z end end
-  return z end  
+  return z end
 
 -- adds(src:s|t,it=NUM()) --> it ;; Update `it` with all items from `src`.
 function adds(src, it)
   it = it or NUM()
-  if type(src) == "string" 
+  if type(src) == "string"
   then for row in csv(src) do add(it,row) end
   else for _,row in pairs(src or {}) do add(it,row) end end
   return it end
@@ -131,24 +131,24 @@ local function scoreGet(data,row,    b,n)
   n = 0
   for _,col in pairs(data.cols.x) do
     b = bin(col, row[col.at])
-    if b ~= "?" then 
+    if b ~= "?" then
       if col.bins[b] then
          n = n + col.bins[b].mu  end end end
   return n end
-      
+
 -- scoreGet(data,row,n) -> nil ;; Add a score `n` to each bin used by this row.
 local function scorePut(data,row,n,     b,y)
   for _,col in pairs(data.cols.x) do
-    b = bin(col, row[col.at]) 
+    b = bin(col, row[col.at])
     if b ~= "?" then
       col.bins[b] = col.bins[b] or NUM(col.at, b)
       add(col.bins[b], n) end end end
 
 -- scoreGuess(data,m,n,rows)-->t ;; sort rows[m] to rows[n] by their guesses
 local function scoreGuess(data,m,n,rows,    t)
-  t = {} 
+  t = {}
   --print((m or 1),min(#rows, n or #rows))
-  for n = (m or 1),min(#rows, n or #rows) do 
+  for n = (m or 1),min(#rows, n or #rows) do
     if n <= #rows then
       t[1+#t] = {scoreGet(data, rows[n]), rows[n]} end end
   return sort(t, function(a,b) return a[1] < b[1] end) end
@@ -158,9 +158,9 @@ local function scoresSeen(data,      t,m,eps)
   t={}; for m,row in pairs(data.rows) do t[1+#t] = disty(data,row) end
   t=sort(t)
   m=#t//10
-  eps = 0.35 * (t[9*m] - t[m])/2.56 
-  print(fmt("%.2f, %.2f, %.2f, %.2f, %.2f; eps= %.2f", 
-                  t[m], t[3*m], t[5*m], t[7*m], t[9*m], eps)) 
+  eps = 0.35 * (t[9*m] - t[m])/2.56
+  print(fmt("%.2f, %.2f, %.2f, %.2f, %.2f; eps= %.2f",
+                  t[m], t[3*m], t[5*m], t[7*m], t[9*m], eps))
   return data,eps end
 
 -- score(data,eps)--> row,n,n ;; Guess whata re good rows in data.
@@ -173,18 +173,18 @@ local function score(data,eps)
   seen  = {}
   n=0;
   for m,row in pairs(data.rows) do
-    if lives < 0 or n >= the.Budget then break end  
+    if lives < 0 or n >= the.Budget then break end
     add(labelled, row)
     scorePut(labelled, row,disty(labelled,row))
     seen[row]=row; n=n+1
-    if m % the.era==0 then 
+    if m % the.era==0 then
       best = scoreGuess(labelled, 1, m+20, data.rows)[1][2]
       if not seen[best] then seen[best]=best; n=n+1 end
       y = disty(data, best)
       if y < besty - eps
       then besty,bestRow = y,best  ; say"!"
       else lives = lives - 1       ; say"."
-      end end end 
+      end end end
   return bestRow, besty,n end 
 
 --## Demos
@@ -196,38 +196,38 @@ egs["-s"] = function(n) math.randomseed(n or the.seed); the.seed =n end
 egs["--the"] = function(_) print(o(the)) end
 egs["--shuffle"] = function(_) print(o(shuffle{10,20,30,40,50})) end
 
-egs["--csv"] = function(_,    n) 
+egs["--csv"] = function(_,    n)
   n=1; for row in csv(the.file) do
-         if n % 25 == 0 then print(o(row)) end 
+         if n % 25 == 0 then print(o(row)) end
          n = n + 1 end end
 
 egs["--num"] = function(_,num)
   num=NUM()
   for _=1,1000 do add(num, box_muller(10,5)) end
-  print(fmt("%.3f %.3f", num.mu, num.sd)) end 
+  print(fmt("%.3f %.3f", num.mu, num.sd)) end
 
-egs["--data"] = function(_) 
-  for n,col in pairs(DATA(the.file).cols.x) do 
+egs["--data"] = function(_)
+  for n,col in pairs(DATA(the.file).cols.x) do
   print(n,o(col)) end end
 
 egs["--disty"]= function(_,    data,num,t)
   data,t = DATA(the.file), {}
   for n,row in pairs(data.rows) do
-    if n % 25 == 0 then t[1+#t] = disty(data,row) end end 
-  print(o(sort(t))) end 
+    if n % 25 == 0 then t[1+#t] = disty(data,row) end end
+  print(o(sort(t))) end
 
-egs["--score"] = function(_,    t,data,eps,y) 
-                   data,eps = scoresSeen(DATA(the.file)) 
+egs["--score"] = function(_,    t,data,eps,y)
+                   data,eps = scoresSeen(DATA(the.file))
                    t={}
                    for n = 1,the.repeats do
                      data.rows  = shuffle(data.rows)
-                     _,y,seen = score(data,eps) 
+                     _,y,seen = score(data,eps)
                      t[n] = 100*y//1 end
                    print("\n"..o(sort(t))) end
 
-egs["--all"] = function(_,   n) 
+egs["--all"] = function(_,   n)
                 n = the.seed
-                for k,_ in pairs(egs) do 
+                for k,_ in pairs(egs) do
                   math.randomseed(n)
                   if k~="--all" then print("\n-----",k); egs[k]() end end end
 
