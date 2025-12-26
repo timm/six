433725e7d5a0513513079f6286a10635a8f8b3dc
timm <timm@ieee.org>
2025-12-27 10:46:23 +1100

    saving

--- src/xai2.py
+++ src/xai2.py
@@ -83,21 +83,23 @@ def _aha(col,u,v):
   v = v if v != "?" else (0 if u>0.5 else 1)
   return abs(u - v)
 
-def peeking(data,rows): # best if rows shuffled
-  d  = clone(data, rows[:the.warm])
-  y  = lambda r: disty(d,rows)
+def peeking(data,rows):            # best if rows arrived shuffled
+  d = clone(data, rows[:the.warm]) # all rows labelled by this function
+  a,z = clone(data),clone(data)    # best, rest labelled rows
+  x = lambda r: distx(d,r,mids(a)) - distx(d,r,mids(z)) # <0 if closest to best
+  y = lambda r: disty(d,r) # distanace of goals to "heaven" (best values)
   d.rows.sorted(key=y)
-  a  = clone(d, d.rows[:the.warm//2])
-  z  = clone(d, d.rows[the.warm//2:])
-  fn = lambda row: distx(d, row, mids(a)) - distx(d, row, mids(z))
-  for row in rows[the.warm:]:
+  adds(d.rows[:the.warm//2], a) 
+  adds(d.rows[the.warm//2:], z) 
+  for r in rows[the.warm:]:
     if d.n >= the.budget: break
-    elif fn(row) < 0: # i.e. closer to a than z
-      add(d, add(a,row))
-      if a.b > d.n**0.5:
+    elif x(r) < 0: 
+      add(d, add(a,r))
+      if a.n > sqrt(d.n): # too many best things
         a.rows.sorted(key=y)
-        add(z, sub(a, a.rows[-1]))
-  return obj(model=fn, labelled=sorted(d.rows,key=fn))
+        add(z, sub(a, a.rows[-1])) # demote worse row in best to rest
+  d.rows.sort(key=x))
+  return obj(model=x, labelled=d)
 
 ## Cutting -------------------------------------------------------------------
 def score(num): return num.mu + sd(num) / (sqrt(num.n) + 1/BIG)
